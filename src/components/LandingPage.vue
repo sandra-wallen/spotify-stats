3823305c5982bed3f56ee4de4ad7435fba63f087
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2025-03-15 23:33:54 +0100

    Update landing page

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,11 @@
+﻿<template>
+  <h1>HEJ</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LandingPage from "@/components/LandingPage.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'landing',
+      component: LandingPage,
     },
     {
       path: '/about',
